67c9f223c5d67f971c95e2dcd1d9dcac3c6d884f
Shlok <shloksuthar0307@gmail.com>
2026-09-21 09:12:31 +0530

    contact

--- ContactBook/settings.py
+++ ContactBook/settings.py
@@ -172,6 +172,23 @@ SITE_ID = 1
 ACCOUNT_LOGIN_METHODS = {'email'}
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
 
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'APP': {
+            'client_id': env('GOOGLE_CLIENT_ID', default=''),
+            'secret': env('GOOGLE_CLIENT_SECRET', default=''),
+            'key': ''
+        }
+    },
+    'github': {
+        'APP': {
+            'client_id': env('GITHUB_CLIENT_ID', default=''),
+            'secret': env('GITHUB_CLIENT_SECRET', default=''),
+            'key': ''
+        }
+    }
+}
+
 # DRF Settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
